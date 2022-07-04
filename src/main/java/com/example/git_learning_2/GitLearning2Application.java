514af107020a51dff75bc63f9b3be1a37aabf82e
unruly_chump <593861484@qq.com>
2022-07-04 09:29:48 +0800

    a new branch

--- src/main/java/com/example/git_learning_2/GitLearning2Application.java
+++ src/main/java/com/example/git_learning_2/GitLearning2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitLearning2Application {
     public static void mai1n(String[] args) {
+        //1111
         SpringApplication.run(GitLearning2Application.class, args);
     }
     public static void main(String[] args) {
