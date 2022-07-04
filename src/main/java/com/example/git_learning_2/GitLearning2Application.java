39706e677bcb2989323474d3bd58f809296c25aa
unruly_chump <593861484@qq.com>
2022-07-04 09:19:01 +0800

    Initial commit

---
+++ src/main/java/com/example/git_learning_2/GitLearning2Application.java
@@ -0,0 +1,13 @@
+package com.example.git_learning_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLearning2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLearning2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_learning_2/GitLearning2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_learning_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLearning2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
